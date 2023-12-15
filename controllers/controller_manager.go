37c07b561cb152435f57cf3326691104d6bc8e0d
Chaz Gregory <speshled@gmail.com>
2023-12-14 23:17:22 -0600

    log controller caps

--- controllers/controller_manager.go
+++ controllers/controller_manager.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"log"
+	"log/slog"
 
 	"github.com/Speshl/pi_drift_wheel/config"
 	"github.com/holoplot/go-evdev"
@@ -51,15 +52,79 @@ func (c *ControllerManager) LoadControllers() error {
 
 		device, err := evdev.Open(inputPath.Path)
 		if err != nil {
-			return fmt.Errorf("Cannot read %s: %w\n", inputPath.Path, err)
+			return fmt.Errorf("failed reading %s: %w\n", inputPath.Path, err)
 		}
 
-		log.Printf("loaded: %s:\t%s\n", inputPath.Path, inputPath.Name)
+		inputId, err := device.InputID()
+		if err != nil {
+			return fmt.Errorf("failed getting input id: %w", err)
+		}
+		uniqueId, err := device.UniqueID()
+		if err != nil {
+			return fmt.Errorf("failed getting unique id: %w", err)
+		}
+
+		slog.Info("loaded device",
+			"name", inputPath.Name,
+			"path", inputPath.Path,
+			"bus_type", inputId.BusType,
+			"vendor", inputId.Vendor,
+			"product", inputId.Product,
+			"version", inputId.Version,
+			"uniqueId", uniqueId,
+		)
+
+		c.showControllerCaps(device)
+
 		c.Controllers = append(c.Controllers, NewController(inputPath, device))
 	}
 	return nil
 }
 
 func (c *ControllerManager) isSupported(name string) bool {
-	return false
+	switch name {
+	case "G27 Racing Wheel", "Arduino LLC Arduino Micro":
+		return true
+	default:
+		return false
+	}
+}
+
+func (c *ControllerManager) showControllerCaps(device *evdev.InputDevice) {
+	for _, t := range device.CapableTypes() {
+		log.Printf("  Event type %d (%s)\n", t, evdev.TypeName(t))
+
+		state, err := device.State(t)
+		if err == nil {
+			for code, value := range state {
+				log.Printf("    Event code %d (%s) state %v\n", code, evdev.CodeName(t, code), value)
+			}
+		}
+
+		if t != evdev.EV_ABS {
+			continue
+		}
+
+		absInfos, err := device.AbsInfos()
+		if err != nil {
+			continue
+		}
+
+		for code, absInfo := range absInfos {
+			log.Printf("    Event code %d (%s)\n", code, evdev.CodeName(t, code))
+			log.Printf("      Value: %d\n", absInfo.Value)
+			log.Printf("      Min: %d\n", absInfo.Minimum)
+			log.Printf("      Max: %d\n", absInfo.Maximum)
+
+			if absInfo.Fuzz != 0 {
+				log.Printf("      Fuzz: %d\n", absInfo.Fuzz)
+			}
+			if absInfo.Flat != 0 {
+				log.Printf("      Flat: %d\n", absInfo.Flat)
+			}
+			if absInfo.Resolution != 0 {
+				log.Printf("      Resolution: %d\n", absInfo.Resolution)
+			}
+		}
+	}
 }
