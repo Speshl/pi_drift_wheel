ef58bfd60d022550d5f643ece66d9444dd338052
Chaz Gregory <speshled@gmail.com>
2024-01-06 14:30:32 -0600

    include linux

--- go-evdev/hello.c
+++ go-evdev/hello.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <linux/input.h>
 
 void Hello(){
     printf("Hello world\n");
 }
+
+uint32_t upload_effect(int fd,  uint8_t *data){
+    struct ff_effect effect = {};
+}
+
+/*
+upload_effect(PyObject *self, PyObject *args)
+{
+    int fd, ret;
+    PyObject* effect_data;
+    ret = PyArg_ParseTuple(args, "iO", &fd, &effect_data);
+    if (!ret) return NULL;
+
+    void* data = PyBytes_AsString(effect_data);
+    struct ff_effect effect = {};
+    memmove(&effect, data, sizeof(struct ff_effect));
+
+    // print_ff_effect(&effect);
+
+    ret = ioctl(fd, EVIOCSFF, &effect);
+    if (ret != 0) {
+        PyErr_SetFromErrno(PyExc_IOError);
+        return NULL;
+    }
+
+    return Py_BuildValue("i", effect.id);
+}
+*/
