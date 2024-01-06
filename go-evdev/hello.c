89609211a65e578a3c817a99319b5c383a160152
Chaz Gregory <speshled@gmail.com>
2024-01-06 16:58:35 -0600

    again

--- go-evdev/hello.c
+++ go-evdev/hello.c
@@ -14,14 +14,14 @@ int upload_effect(uintptr_t fd,  void *effect_data){
 
     effect.type = FF_CONSTANT;
     effect.id = -1;           // Unique ID for the effect (use -1 for auto-assignment)
-    effect.direction = 0;     // Direction of the effect (0 for omni-directional)
+    effect.direction = 20000;     // Direction of the effect (0 for omni-directional)
     effect.trigger.button = 0; // Button that triggers the effect (0 for no button)
     effect.trigger.interval = 0; // Interval between triggers (0 for continuous)
-    effect.replay.length = 100;  // Duration of the effect in milliseconds
+    effect.replay.length = 0;  // Duration of the effect in milliseconds
     effect.replay.delay = 0;     // Delay before replaying the effect (0 for no delay)
 
     // Parameters specific to the constant effect
-    effect.u.constant.level = 0x8000; // Example: Constant force level (signed 16-bit)
+    effect.u.constant.level = 32767; // Example: Constant force level (signed 16-bit)
     int error = ioctl(fd, EVIOCSFF, &effect);
     if(error != 0){
         return -2;
@@ -33,7 +33,7 @@ int upload_effect(uintptr_t fd,  void *effect_data){
     gettimeofday(&tval, 0);
     event.input_event_usec = tval.tv_usec;
     event.input_event_sec = tval.tv_sec;
-    event.type = 0x15;
+    event.type = EV_FF;
     event.code = effect.id;
     event.value = 1;
 
