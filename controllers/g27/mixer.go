0d6b0475da366bba426c83ebf35b430909e0c0bb
Chaz Gregory <speshled@gmail.com>
2024-02-04 13:00:17 -0600

    again

--- controllers/g27/mixer.go
+++ controllers/g27/mixer.go
@@ -178,12 +178,12 @@ func getEscValueReverse(inputs []models.Input, mixState models.MixState) (uint16
 			//mixState.Esc = "brake"
 			//slog.Info("no input in forward, switch to brakes", "esc", returnValue, "state", mixState.Esc)
 		case "brake":
-			returnValue = uint16(sbus.MidValue) + 50
+			returnValue = uint16(sbus.MidValue) + 150
 			returnPriority = 20
 			mixState.Esc = "forward"
 			slog.Info("no input in brake in reverse", "esc", returnValue, "state", mixState.Esc)
 		case "reverse":
-			returnValue = uint16(sbus.MidValue) + 50
+			returnValue = uint16(sbus.MidValue) + 150
 			returnPriority = 20
 			mixState.Esc = "forward"
 			slog.Info("no input in reverse", "esc", returnValue, "state", mixState.Esc)
