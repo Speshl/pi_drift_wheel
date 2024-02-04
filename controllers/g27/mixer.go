f2e96a0b70d65e3e69642cabc0fd1ddebfd261a4
Chaz Gregory <speshled@gmail.com>
2024-02-04 12:08:31 -0600

    again

--- controllers/g27/mixer.go
+++ controllers/g27/mixer.go
@@ -125,12 +125,12 @@ func getEscValueReverse(inputs []models.Input, mixState models.MixState) (uint16
 		switch mixState.Esc {
 		case "forward": //go to brakes
 			returnValue = uint16(sbus.MidValue) - 50
-			returnPriority = 3
+			returnPriority = 5
 			mixState.Esc = "brake"
 			slog.Info("to brake from forward in reverse", "esc", returnValue, "state", mixState.Esc)
 		case "brake": //go to reverse
 			returnValue = uint16(sbus.MidValue)
-			returnPriority = 3
+			returnPriority = 5
 			mixState.Esc = "reverse"
 			slog.Info("to reverse from brake in reverse", "esc", returnValue, "state", mixState.Esc)
 		case "reverse": //set reverse value
@@ -308,155 +308,155 @@ func getEscValueWithoutGears(inputs []models.Input, mixState models.MixState) (u
 	return returnValue, returnPriority, mixState
 }
 
-func getEscValueOld(inputs []models.Input, mixState models.MixState, opts models.ControllerOptions) (uint16, int, models.MixState) {
-	returnValue := uint16(sbus.MinValue)
-	returnPriority := 0
+// func getEscValueOld(inputs []models.Input, mixState models.MixState, opts models.ControllerOptions) (uint16, int, models.MixState) {
+// 	returnValue := uint16(sbus.MinValue)
+// 	returnPriority := 0
 
-	gasChange := models.GetInputChangeAmount(inputs[1])
-	brakeChange := models.GetInputChangeAmount(inputs[2])
-	//ESC Value
-	if opts.UseHPattern {
-		for i := 10; i < 20; i++ {
-			if inputs[i].Value > inputs[i].Min {
-				if i == 19 {
-					mixState.Gear = -1 //reverse
-				} else if i > 15 {
-					mixState.Gear = 0 //set neutral when unsupported gear pressed
-					continue
-				} else {
-					mixState.Gear = i - 9 //supported gears
-				}
-				break //only 1 gear can be active at a time, so stop when one found
-			} else if i == 19 {
-				mixState.Gear = 0 //no gear button pressed, set to neutral
-			}
-		}
+// 	gasChange := models.GetInputChangeAmount(inputs[1])
+// 	brakeChange := models.GetInputChangeAmount(inputs[2])
+// 	//ESC Value
+// 	if opts.UseHPattern {
+// 		for i := 10; i < 20; i++ {
+// 			if inputs[i].Value > inputs[i].Min {
+// 				if i == 19 {
+// 					mixState.Gear = -1 //reverse
+// 				} else if i > 15 {
+// 					mixState.Gear = 0 //set neutral when unsupported gear pressed
+// 					continue
+// 				} else {
+// 					mixState.Gear = i - 9 //supported gears
+// 				}
+// 				break //only 1 gear can be active at a time, so stop when one found
+// 			} else if i == 19 {
+// 				mixState.Gear = 0 //no gear button pressed, set to neutral
+// 			}
+// 		}
 
-		if gasChange > brakeChange && gasChange > 10 { //throttle is pressed more than brake
-			if mixState.Gear == 0 { //Neutral so keep esc at center value
-				returnValue = uint16(sbus.MidValue)
-				slog.Info("neutral")
-			} else if mixState.Gear == -1 { //Reverse
-				switch mixState.Esc {
-				case "forward": //Going to reverse from forward needs to turn on brakes first then reverse to get esc into reverse mode
-					returnValue = uint16(sbus.MinValue) //full brakes
-					returnPriority = 6
-					mixState.Esc = "brake"
-					slog.Info("setting esc to full brakes before switching to reverse")
+// 		if gasChange > brakeChange && gasChange > 10 { //throttle is pressed more than brake
+// 			if mixState.Gear == 0 { //Neutral so keep esc at center value
+// 				returnValue = uint16(sbus.MidValue)
+// 				slog.Info("neutral")
+// 			} else if mixState.Gear == -1 { //Reverse
+// 				switch mixState.Esc {
+// 				case "forward": //Going to reverse from forward needs to turn on brakes first then reverse to get esc into reverse mode
+// 					returnValue = uint16(sbus.MinValue) //full brakes
+// 					returnPriority = 6
+// 					mixState.Esc = "brake"
+// 					slog.Info("setting esc to full brakes before switching to reverse")
 
-				case "brake":
-					returnValue = uint16(sbus.MidValue) //set mid to get the esc out of brake
-					returnPriority = 6
-					mixState.Esc = "reverse"
-					slog.Info("setting esc center to get out of brake and prepare for reverse")
-				case "reverse":
-					value := models.MapToRange( //Map throttle to bottom half of esc channel
-						inputs[1].Value,
-						inputs[1].Min,
-						inputs[1].Max,
-						sbus.MinValue,
-						sbus.MidValue,
-					)
-					returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
-					slog.Info("setting reverse in reverse", "value", returnValue)
-				}
+// 				case "brake":
+// 					returnValue = uint16(sbus.MidValue) //set mid to get the esc out of brake
+// 					returnPriority = 6
+// 					mixState.Esc = "reverse"
+// 					slog.Info("setting esc center to get out of brake and prepare for reverse")
+// 				case "reverse":
+// 					value := models.MapToRange( //Map throttle to bottom half of esc channel
+// 						inputs[1].Value,
+// 						inputs[1].Min,
+// 						inputs[1].Max,
+// 						sbus.MinValue,
+// 						sbus.MidValue,
+// 					)
+// 					returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
+// 					slog.Info("setting reverse in reverse", "value", returnValue)
+// 				}
 
-			} else if mixState.Gear > 0 && mixState.Gear <= 6 {
-				value := int(float64(inputs[1].Value) / float64(6) * float64(mixState.Gear)) //Scale throttle to gear
-				if mixState.Gear == 6 {
-					value = inputs[1].Value //let top gear have full range without rounding issues
-				}
+// 			} else if mixState.Gear > 0 && mixState.Gear <= 6 {
+// 				value := int(float64(inputs[1].Value) / float64(6) * float64(mixState.Gear)) //Scale throttle to gear
+// 				if mixState.Gear == 6 {
+// 					value = inputs[1].Value //let top gear have full range without rounding issues
+// 				}
 
-				value = models.MapToRange(
-					value,
-					inputs[1].Min,
-					inputs[1].Max,
-					sbus.MidValue,
-					sbus.MaxValue,
-				)
-				returnValue = uint16(value)
-				if returnValue > uint16(sbus.MidValue) {
-					mixState.Esc = "forward"
-				}
-				slog.Info("setting forward in forward", "value", returnValue)
-			} else {
-				slog.Warn("gear out of bounds")
-			}
-		} else if brakeChange > gasChange && brakeChange > 10 { //Brake is pressed more than throttle
-			switch mixState.Esc {
-			case "forward":
-				value := models.MapToRangeWithDeadzoneLow(
-					inputs[2].Value,
-					inputs[2].Min,
-					inputs[2].Max,
-					sbus.MinValue,
-					sbus.MidValue,
-					2,
-				)
-				returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
-				if returnValue < uint16(sbus.MidValue) {
+// 				value = models.MapToRange(
+// 					value,
+// 					inputs[1].Min,
+// 					inputs[1].Max,
+// 					sbus.MidValue,
+// 					sbus.MaxValue,
+// 				)
+// 				returnValue = uint16(value)
+// 				if returnValue > uint16(sbus.MidValue) {
+// 					mixState.Esc = "forward"
+// 				}
+// 				slog.Info("setting forward in forward", "value", returnValue)
+// 			} else {
+// 				slog.Warn("gear out of bounds")
+// 			}
+// 		} else if brakeChange > gasChange && brakeChange > 10 { //Brake is pressed more than throttle
+// 			switch mixState.Esc {
+// 			case "forward":
+// 				value := models.MapToRangeWithDeadzoneLow(
+// 					inputs[2].Value,
+// 					inputs[2].Min,
+// 					inputs[2].Max,
+// 					sbus.MinValue,
+// 					sbus.MidValue,
+// 					2,
+// 				)
+// 				returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
+// 				if returnValue < uint16(sbus.MidValue) {
 
-					mixState.Esc = "brake"
-					returnPriority = 6
-					slog.Info("to brake from forward", "esc", returnValue, "base", inputs[2].Value)
-				} else {
-					slog.Info("braking from forward", "esc", returnValue, "base", inputs[2].Value)
-				}
-			case "brake":
-				value := models.MapToRangeWithDeadzoneLow(
-					inputs[2].Value,
-					inputs[2].Min,
-					inputs[2].Max,
-					sbus.MinValue,
-					sbus.MidValue,
-					2,
-				)
-				returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
-				if returnValue < uint16(sbus.MidValue) {                    //brakes not/or barely pushed
-					returnValue = uint16(sbus.MidValue + 100) //set enough forward keep esc out of reverse
-					returnPriority = 6
-					mixState.Esc = "forward"
-					slog.Info("keeping brakes from going to reverse, by setting slightly forward", "esc", returnValue)
-				} else {
-					slog.Info("brakes in brake state", "esc", returnValue)
-				}
-			case "reverse":
-				returnValue = uint16(sbus.MidValue + 50) //set enough forward to get the esc out of reverse
-				returnPriority = 6
-				mixState.Esc = "forward"
-				slog.Info("getting esc out of reverse before pressing the brakes", "esc", returnValue)
-			}
-		} else {
-			returnValue = uint16(sbus.MidValue)
-			//slog.Info("no peddals", "throttle", gasChange, "brake", brakeChange)
-		}
-	} else { //map without using gear selections
-		if gasChange > brakeChange && gasChange > 10 { //throttle is pressed more than brake
-			returnValue = uint16(models.MapToRangeWithDeadzoneLow(
-				inputs[1].Value,
-				inputs[1].Min,
-				inputs[1].Max,
-				sbus.MidValue,
-				sbus.MaxValue,
-				2,
-			))
-			slog.Info("no gear gas")
-		} else if brakeChange > gasChange && brakeChange > 10 { //brake pressed more or equal to throttle
-			value := models.MapToRangeWithDeadzoneLow(
-				inputs[2].Value,
-				inputs[2].Min,
-				inputs[2].Max,
-				sbus.MinValue,
-				sbus.MidValue,
-				2,
-			)
-			returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
-			slog.Info("no gear brake")
-		} else {
-			returnValue = uint16(sbus.MidValue)
-			//slog.Info("no gears no peddals", "throttle", gasChange, "brake", brakeChange)
-		}
-	}
+// 					mixState.Esc = "brake"
+// 					returnPriority = 6
+// 					slog.Info("to brake from forward", "esc", returnValue, "base", inputs[2].Value)
+// 				} else {
+// 					slog.Info("braking from forward", "esc", returnValue, "base", inputs[2].Value)
+// 				}
+// 			case "brake":
+// 				value := models.MapToRangeWithDeadzoneLow(
+// 					inputs[2].Value,
+// 					inputs[2].Min,
+// 					inputs[2].Max,
+// 					sbus.MinValue,
+// 					sbus.MidValue,
+// 					2,
+// 				)
+// 				returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
+// 				if returnValue < uint16(sbus.MidValue) {                    //brakes not/or barely pushed
+// 					returnValue = uint16(sbus.MidValue + 100) //set enough forward keep esc out of reverse
+// 					returnPriority = 6
+// 					mixState.Esc = "forward"
+// 					slog.Info("keeping brakes from going to reverse, by setting slightly forward", "esc", returnValue)
+// 				} else {
+// 					slog.Info("brakes in brake state", "esc", returnValue)
+// 				}
+// 			case "reverse":
+// 				returnValue = uint16(sbus.MidValue + 50) //set enough forward to get the esc out of reverse
+// 				returnPriority = 6
+// 				mixState.Esc = "forward"
+// 				slog.Info("getting esc out of reverse before pressing the brakes", "esc", returnValue)
+// 			}
+// 		} else {
+// 			returnValue = uint16(sbus.MidValue)
+// 			//slog.Info("no peddals", "throttle", gasChange, "brake", brakeChange)
+// 		}
+// 	} else { //map without using gear selections
+// 		if gasChange > brakeChange && gasChange > 10 { //throttle is pressed more than brake
+// 			returnValue = uint16(models.MapToRangeWithDeadzoneLow(
+// 				inputs[1].Value,
+// 				inputs[1].Min,
+// 				inputs[1].Max,
+// 				sbus.MidValue,
+// 				sbus.MaxValue,
+// 				2,
+// 			))
+// 			slog.Info("no gear gas")
+// 		} else if brakeChange > gasChange && brakeChange > 10 { //brake pressed more or equal to throttle
+// 			value := models.MapToRangeWithDeadzoneLow(
+// 				inputs[2].Value,
+// 				inputs[2].Min,
+// 				inputs[2].Max,
+// 				sbus.MinValue,
+// 				sbus.MidValue,
+// 				2,
+// 			)
+// 			returnValue = uint16(sbus.MidValue - value + sbus.MinValue) //invert since on bottom half
+// 			slog.Info("no gear brake")
+// 		} else {
+// 			returnValue = uint16(sbus.MidValue)
+// 			//slog.Info("no gears no peddals", "throttle", gasChange, "brake", brakeChange)
+// 		}
+// 	}
 
-	return returnValue, returnPriority, mixState
-}
+// 	return returnValue, returnPriority, mixState
+// }
