f61122402d72b50c375cbea7bf51eb6dc978c5bf
Chaz Gregory <speshled@gmail.com>
2024-02-20 16:06:53 -0600

    again

--- controllers/controller_manager.go
+++ controllers/controller_manager.go
@@ -161,7 +161,7 @@ func (c *ControllerManager) GetMixedFrame() (sbus.SBusFrame, error) {
 	}
 
 	mixedInputs := c.Controllers[0].GetRawInputs()
-	slog.Info("initial inputs device", "name", c.Controllers[0], "brake", mixedInputs[2])
+	slog.Info("initial inputs device", "name", c.Controllers[0].Name, "brake", mixedInputs[2])
 
 	for i := 1; i < len(c.Controllers); i++ {
 		i := i
