ba4a44d53c76f8c2dce66ce23fb5163c21946b92
Chaz Gregory <speshled@gmail.com>
2024-02-20 16:33:19 -0600

    again

--- controllers/handbrake_diy/key_map.go
+++ controllers/handbrake_diy/key_map.go
@@ -19,5 +19,17 @@ func GetKeyMap() map[string]models.Mapping {
 		Label:    "handbrake",
 	}
 
+	keyMap["3:2"] = models.Mapping{
+		Label:    "empty",
+		CodeName: "ABS_Z",
+		Type:     3,
+		Code:     2,
+		RawInput: 1,
+		Min:      0,
+		Max:      255,
+		Rests:    "low",
+		Inverted: true,
+	}
+
 	return keyMap
 }
